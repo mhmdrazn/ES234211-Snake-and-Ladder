1e59bed92d49d906f89d7bf0b5935c11c4ccbc60
Muhammad Razan <140767783+mhmdrazn@users.noreply.github.com>
2024-07-02 11:31:14 +0700

    Update Todboxes.java

--- src/Todboxes.java
+++ src/Todboxes.java
@@ -1,18 +1,3 @@
-/**
- * -----------------------------------------------------
- * ES234211 - Programming Fundamental
- * Genap - 2023/2024
- * Group Capstone Project: Snake and Ladder Game
- * -----------------------------------------------------
- * Class    : C
- * Group    : 03
- * Members  :
- * 1. 5026231016 - Fathan Maulana Prasetya
- * 2. 5026231031 - Marco Indrajaya
- * 3. 5026231174 - Muhammad Razan Parisya Putra
- * ------------------------------------------------------
- */
-
 import java.util.Random;
 
 public class Todboxes {
